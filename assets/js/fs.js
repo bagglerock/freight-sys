a87c677174b1205968420261926f18e7da81bd10
Oscar Villalta <bagglerock@Oscars-MBP.fios-router.home>
2018-03-09 03:24:28 -0500

    done for now

--- assets/js/fs.js
+++ assets/js/fs.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
-    $("main").slideDown(1000);
+    $("#main").slideDown(1000);
     $("#mission").slideDown(1000);
     
-})
-
-$(window).resize(function(){
-    var xHeight = $('.footer').height();
-    $('body').css('margin-bottom',xHeight + 'px');
-})
+});
