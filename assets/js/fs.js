f9eb3f5f67b0237c090ffaee07c45844a004c404
Oscar Villalta <bagglerock@Oscars-MacBook-Pro.local>
2018-03-10 12:17:07 -0500

    adjusted the overlay text when the page is small and added slidedowns to all pages

--- assets/js/fs.js
+++ assets/js/fs.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $("#main").slideDown(1000);
-    $("#mission").slideDown(1000);
-    
+    $("#about").slideDown(1000);
+    $("#contact").slideDown(1000);
+    $("#location").slideDown(1000);
 });
